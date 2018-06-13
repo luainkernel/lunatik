a3d8ba92391e6ad54c1e595aa95abc0f8686c7b0
Pedro Tammela <pctammela@getcujo.com>
2018-06-13 19:21:13 +0000

    RABID-85 use correct allocation flag under different half contexts

--- lua/luaconf.h
+++ lua/luaconf.h
@@ -855,7 +855,12 @@ static inline int time(void *p)
 #endif
 
 #define free 			kfree
-#define realloc(a, b) 		krealloc(a, b, GFP_KERNEL)
+#include <linux/hardirq.h>
+static inline void *realloc(const void *p, size_t new_size)
+{
+	gfp_t flag = in_interrupt() ? GFP_ATOMIC : GFP_KERNEL;
+	return krealloc(p, new_size, flag);
+}
 
 /* signal.h */
 #define l_signalT	lu_byte
