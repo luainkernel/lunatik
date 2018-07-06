35e7c5d5787e95f4af768c71a9ab610d0d8bffb2
Tangent <tcz717@hotmail.com>
2018-07-06 00:24:52 -0400

    Fix proxy demo bugs

--- socket/examples/proxy.lua
+++ socket/examples/proxy.lua
@@ -5,8 +5,8 @@ Head =
     data.layout {
     vn = {0, 8},
     cd = {8, 8},
-    dstport = {16, 16, "host"},
-    dstip = {32, 32, "host"}
+    dstport = {16, 16, "number", "net"},
+    dstip = {32, 32, "number", "net"}
 }
 Command = {
     connect = 1,
@@ -32,18 +32,29 @@ function Client:handle()
     local _, size = self.socket:recv(self.buf)
 
     assert(size > REQUEST_MIN)
-    -- assert(self.buf:segment(size - 1, 1):layout {null = {0, 8}} == 0)
+    print("received size: " .. size)
 
-    req = self.buf:layout(Head)
+    local req_size = nil
+    for i = REQUEST_MIN, size do
+        local char = self.buf:layout({c = {(i - 1) * 8, 8}}).c
+        if (char == 0) then
+            req_size = i
+            print("request size: " .. i)
+            break
+        end
+    end
     -- only support SOCKS4 and CONNECT command now
+    local req = self.buf:layout(Head)
     if req.vn ~= 4 or req.cd ~= Command.connect then
-        print("Not SOCKS4 or is bind")
+        print("Not SOCKS4 or not connect")
+        print("version: " .. req.vn .. " command: " .. req.cd)
         self:response(Command.rejected, req.dstport, req.dstip)
         self.socket:close()
 
         return
     end
 
+    print("try establish connection to " .. req.dstip .. ":" .. req.dstport)
     local sock = socket.new("i", "t")
     local state, err = pcall(sock.connect, sock, req.dstip, req.dstport)
     if not state then
@@ -54,18 +65,39 @@ function Client:handle()
         return
     end
 
+    print("establish connection ok")
     self:response(Command.granted, req.dstport, req.dstip)
     self.target_socket = sock
     pcall(
         -- only support request/response mode
         function()
+            if req_size and req_size < size then
+                size = sock:send(self.buf:segment(req_size, size - req_size))
+                print("tail request size: " .. size)
+
+                _, size = sock:recv(self.buf)
+                self.socket:send(self.buf:segment(0, size))
+
+                print("response size: " .. size)
+            end
             while true do
                 _, size = self.socket:recv(self.buf)
+                if size == 0 then
+                    print("client disconnected")
+                    break
+                end
                 sock:send(self.buf:segment(0, size))
+                print("request size: " .. size)
+
+                _, size = sock:recv(self.buf)
+                self.socket:send(self.buf:segment(0, size))
+
+                print("response size: " .. size)
             end
         end
     )
-    sock:clsoe()
+    print("stop handle")
+    sock:close()
     self.socket:close()
 end
 
@@ -82,5 +114,7 @@ local server = socket.new("i", "t")
 server:bind("0.0.0.0", PORT)
 server:listen(5)
 
+print("SOCKv4 start")
 client = Client:new(server:accept())
 client:handle()
+server:close()
