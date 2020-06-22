b81dea8eecbc5551456b0867fb441430be8ab41a
Victor Nogueira <victor.nogueira@ring-0.io>
2020-06-22 17:01:47 -0300

    Fix redefinition of time_t bug

--- lua/luaconf.h
+++ lua/luaconf.h
@@ -842,18 +842,18 @@ extern void longjmp(luai_jmpbuf);
 #include <linux/ktime.h>
 #if LINUX_VERSION_CODE < KERNEL_VERSION(5,6,0)
 #include <linux/time.h>
-typedef struct timespec time_t;
+typedef struct timespec lunatik_time_t;
 #define luai_time(t)		getnstimeofday(&t)
 #elif defined(LUA_32BITS)
 #error "kernel versions >= 5.6.0 do not support 32-bit time values"
 #else
-typedef struct timespec64 time_t;
+typedef struct timespec64 lunatik_time_t;
 #define luai_time(t)		ktime_get_real_ts64(&t)
 #endif
 
 static inline int time(void *p)
 {
-  time_t t;
+  lunatik_time_t t;
   ((void) p);
   luai_time(t);
   return t.tv_sec;
