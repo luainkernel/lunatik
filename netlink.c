8cd6cdc0ecbfc34e9d2a92bf7bf6e46c5dae122d
Matheus Rodrigues <matheussr61@gmail.com>
2020-07-27 11:46:32 -0300

    Update copyright

--- netlink.c
+++ netlink.c
@@ -1,6 +1,5 @@
 /*
  * Copyright (C) 2020  Matheus Rodrigues <matheussr61@gmail.com>
- * Copyright (C) 2017-2019  CUJO LLC
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
