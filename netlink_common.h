b325f156f246ac45698ddabf8e8fa70a8d0bb4d3
Matheus Rodrigues <matheussr61@gmail.com>
2020-09-04 12:34:31 +0000

    fixup! Netlink implementation

--- netlink_common.h
+++ netlink_common.h
@@ -24,16 +24,16 @@ extern struct genl_family lunatik_family;
 #include <net/genetlink.h>
 #endif /* _KERNEL */
 
-#define LUNATIK_FRAGMENT_SIZE (3000) // TODO Find, a size more precise
-#define DELIMITER 3 //How many delimiters will be necessary in each part of the message
+#define LUNATIK_FRAGMENT_SIZE	(3000) /* TODO Find, a more precise size */
+#define DELIMITER	(3) /* How many delimiters will be necessary in each part of the message */
 
 /*Lunatik generic netlink protocol flags*/
-#define LUNATIK_INIT	0x01 /* Initializes the needed variables for script execution */
-#define LUNATIK_MULTI	0x02 /* A Fragment of a multipart message					  */
-#define LUNATIK_DONE	0x04 /* Last message of a multipart message 				  */
+#define LUNATIK_INIT	(0x01) /* Initializes the needed variables for script execution */
+#define LUNATIK_MULTI	(0x02) /* A Fragment of a multipart message						*/
+#define LUNATIK_DONE	(0x04) /* Last message of a multipart message					*/
 
-#define LUNATIK_FAMILY "lunatik_family"
-#define LUNATIK_NLVERSION 1
+#define LUNATIK_FAMILY	("lunatik_family")
+#define LUNATIK_NLVERSION	(1)
 
 enum lunatik_operations {
 	CREATE_STATE = 1, /* Starts at 1 because 0 is used by generic netlink */
@@ -64,7 +64,7 @@ enum lunatik_attrs {
 	CURR_ALLOC,
 	STATE_NOT_FOUND,
 	ATTRS_COUNT
-#define ATTRS_MAX (ATTRS_COUNT - 1)
+#define ATTRS_MAX	(ATTRS_COUNT - 1)
 };
 
 #endif /* NETLINK_COMMOM_H */
