488c04aaa5be04a254a690798d6a87f3eeeaaf6e
Lourival Vieira Neto <lourival.neto@ring-0.io>
2023-11-23 22:47:46 +0000

    add luadata

---
+++ lib/luadata.c
@@ -0,0 +1,193 @@
+/*
+* Copyright (c) 2023 ring-0 Ltda.
+*
+* Permission is hereby granted, free of charge, to any person obtaining
+* a copy of this software and associated documentation files (the
+* "Software"), to deal in the Software without restriction, including
+* without limitation the rights to use, copy, modify, merge, publish,
+* distribute, sublicense, and/or sell copies of the Software, and to
+* permit persons to whom the Software is furnished to do so, subject to
+* the following conditions:
+*
+* The above copyright notice and this permission notice shall be
+* included in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
+
+#include <lunatik.h>
+
+typedef struct luadata_s {
+	char *ptr;
+	size_t size;
+	bool free;
+} luadata_t;
+
+#define LUADATA_MT	"data"
+
+#define LUADATA_NUMBER_SZ	(sizeof(lua_Integer))
+
+static void *luadata_realloc(lua_State *L, void *ptr, size_t size)
+{
+	void *ud = NULL;
+	lua_Alloc alloc = lua_getallocf(L, &ud);
+	return alloc(ud, ptr, LUA_TNONE, size);
+}
+
+#define luadata_malloc(L, s)	(luadata_realloc((L), NULL, (s)))
+#define luadata_free(L, p)	(luadata_realloc((L), (p), 0))
+
+static int luadata_new(lua_State *L)
+{
+	size_t size = (size_t)luaL_checkinteger(L, 1);
+	luadata_t *data = (luadata_t *)lua_newuserdatauv(L, sizeof(luadata_t), 0);
+
+	data->ptr = luadata_malloc(L, size);
+	if (data->ptr == NULL)
+		luaL_error(L, "not enough memory");
+
+	data->size = size;
+	data->free = true;
+
+	luaL_setmetatable(L, LUADATA_MT);
+	return 1; /* userdata */
+}
+
+static inline luadata_t *luadata_checkdata(lua_State *L, lua_Integer *offset, lua_Integer length)
+{
+	luadata_t *data = (luadata_t *)luaL_checkudata(L, 1, LUADATA_MT);
+	luaL_argcheck(L, data->ptr != NULL, 1, "null-pointer dereference");
+	*offset = luaL_checkinteger(L, 2);
+	luaL_argcheck(L, *offset >= 0 && length > 0 && *offset + length <= data->size, 2, "out of bounds");
+	return data;
+}
+
+static int luadata_getnumber(lua_State *L)
+{
+	lua_Integer offset;
+	luadata_t *data = luadata_checkdata(L, &offset, LUADATA_NUMBER_SZ);
+
+	lua_pushinteger(L, *(lua_Integer *)(data->ptr + offset));
+	return 1;
+}
+
+static int luadata_setnumber(lua_State *L)
+{
+	lua_Integer offset;
+	luadata_t *data = luadata_checkdata(L, &offset, LUADATA_NUMBER_SZ);
+
+	*(lua_Integer *)(data->ptr + offset) = luaL_checkinteger(L, 3);
+	return 0;
+}
+
+static int luadata_getbyte(lua_State *L)
+{
+	lua_Integer offset;
+	luadata_t *data = luadata_checkdata(L, &offset, 1);
+
+	lua_pushinteger(L, (lua_Integer)*(data->ptr + offset));
+	return 1;
+}
+
+static int luadata_setbyte(lua_State *L)
+{
+	lua_Integer offset;
+	luadata_t *data = luadata_checkdata(L, &offset, 1);
+
+	*(data->ptr + offset) = luaL_checkinteger(L, 3);
+	return 0;
+}
+
+static int luadata_getstring(lua_State *L)
+{
+	lua_Integer offset;
+	lua_Integer length = luaL_checkinteger(L, 3);
+	luadata_t *data = luadata_checkdata(L, &offset, length);
+
+	lua_pushlstring(L, data->ptr + offset, length);
+	return 1;
+}
+
+static int luadata_setstring(lua_State *L)
+{
+	lua_Integer offset;
+	size_t length;
+	const char *str = luaL_checklstring(L, 3, &length); 
+	luadata_t *data = luadata_checkdata(L, &offset, (lua_Integer)length);
+
+	memcpy(data->ptr + offset, str, length);
+	return 0;
+}
+
+static int luadata_delete(lua_State *L)
+{
+	luadata_t *data = (luadata_t *)luaL_checkudata(L, 1, LUADATA_MT);
+
+	if (data->ptr != NULL) { 
+		if (data->free)
+			luadata_free(L, data->ptr);
+		data->ptr = NULL;
+	}
+	return 0;
+}
+
+static const luaL_Reg luadata_lib[] = {
+	{"new", luadata_new},
+	{"delete", luadata_delete},
+	{"getnumber", luadata_getnumber},
+	{"setnumber", luadata_setnumber},
+	{"getbyte", luadata_getbyte},
+	{"setbyte", luadata_setbyte},
+	{"getstring", luadata_getstring},
+	{"setstring", luadata_setstring},
+	{NULL, NULL}
+};
+
+static const luaL_Reg luadata_mt[] = {
+	{"__gc", luadata_delete},
+	{"__close", luadata_delete},
+	{"delete", luadata_delete},
+	{"getnumber", luadata_getnumber},
+	{"setnumber", luadata_setnumber},
+	{"getbyte", luadata_getbyte},
+	{"setbyte", luadata_setbyte},
+	{"getstring", luadata_getstring},
+	{"setstring", luadata_setstring},
+	{NULL, NULL}
+};
+
+static const lunatik_class_t luadata_class = {
+	.name = LUADATA_MT,
+	.methods = luadata_mt,
+};
+
+LUNATIK_NEWLIB(data, luadata_lib, &luadata_class, NULL, true);
+
+static int __init luadata_init(void)
+{
+	return 0;
+}
+
+static void __exit luadata_exit(void)
+{
+}
+
+module_init(luadata_init);
+module_exit(luadata_exit);
+MODULE_LICENSE("Dual MIT/GPL");
+MODULE_AUTHOR("Lourival Vieira Neto <lourival.neto@ring-0.io>");
+
