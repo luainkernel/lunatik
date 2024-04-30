8de7d9aba95dc01d42f0df365b0f2a1b9685425f
Lourival Vieira Neto <lourival.neto@ring-0.io>
2024-04-30 11:47:48 +0000

    add luarcu C API

--- lib/luarcu.c
+++ lib/luarcu.c
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2023 ring-0 Ltda.
+* Copyright (c) 2023-2024 ring-0 Ltda.
 *
 * Permission is hereby granted, free of charge, to any person obtaining
 * a copy of this software and associated documentation files (the
@@ -34,7 +34,7 @@
 
 #include <lunatik.h>
 
-#define LUARCU_DEFAULT_SIZE	(256)
+#include "luarcu.h"
 
 typedef struct luarcu_entry_s {
 	const char *key;
@@ -108,11 +108,9 @@ static int luarcu_replace(luarcu_entry_t *old, lunatik_object_t *object)
 
 static int luarcu_add(luarcu_table_t *table, unsigned int index, const char *key, size_t keylen, lunatik_object_t *object)
 {
-	char *entry_key;
+	char *entry_key = (char *)kmalloc(sizeof(char) * (keylen + 1), GFP_ATOMIC);
 	luarcu_entry_t *entry;
 
-	keylen++; /* null-terminated */
-	entry_key = (char *)kmalloc(sizeof(const char) * keylen, GFP_ATOMIC);
 	if (entry_key == NULL)
 		return -1;
 
@@ -123,6 +121,7 @@ static int luarcu_add(luarcu_table_t *table, unsigned int index, const char *key
 	}
 
 	strncpy(entry_key, key, keylen);
+	entry_key[keylen] = '\0';
 	hlist_add_head_rcu(&entry->hlist, table->hlist + index);
 	return 0;
 }
@@ -159,35 +158,43 @@ static int luarcu_cloneobject(lua_State *L)
 	return 1;
 }
 
-static int luarcu_index(lua_State *L)
+lunatik_object_t *luarcu_gettable(lunatik_object_t *table, const char *key, size_t keylen)
 {
-	luarcu_table_t *table = luarcu_checktable(L, 1);
+	luarcu_table_t *_table = (luarcu_table_t *)table->private;
+	unsigned int index = luarcu_hash(_table, key, keylen);
+	lunatik_object_t *value = NULL;
 	luarcu_entry_t *entry;
-	lunatik_object_t *object;
-	size_t keylen;
-	const char *key = luaL_checklstring(L, 2, &keylen);
-	unsigned int index = luarcu_hash(table, key, keylen);
 
 	rcu_read_lock();
-	entry = luarcu_lookup(table, index, key, keylen);
-	if (entry == NULL) {
-		rcu_read_unlock();
-		lua_pushnil(L);
-		return 1;
+	entry = luarcu_lookup(_table, index, key, keylen);
+	if (entry != NULL) {
+		/* entry might be released after rcu_read_unlock */
+		value = entry->object; /* thus we need to store object pointer */
+		lunatik_getobject(value);
 	}
-
-	/* entry might be released after rcu_read_unlock */
-	object = entry->object; /* thus we need to store object pointer */
-	lunatik_getobject(object);
 	rcu_read_unlock();
+	return value;
+}
+EXPORT_SYMBOL(luarcu_gettable);
 
-	lua_pushcfunction(L, luarcu_cloneobject);
-	lua_pushlightuserdata(L, object);
-	if (lua_pcall(L, 1, 1, 0) != LUA_OK) {
-		lunatik_putobject(object);
-		lua_error(L);
+static int luarcu_index(lua_State *L)
+{
+	lunatik_object_t *table = lunatik_checkobject(L, 1);
+	size_t keylen;
+	const char *key = luaL_checklstring(L, 2, &keylen);
+	lunatik_object_t *value = luarcu_gettable(table, key, keylen);
+
+	if (value == NULL)
+		lua_pushnil(L);
+	else {
+		lua_pushcfunction(L, luarcu_cloneobject);
+		lua_pushlightuserdata(L, value);
+		if (lua_pcall(L, 1, 1, 0) != LUA_OK) {
+			lunatik_putobject(value);
+			lua_error(L);
+		}
 	}
-	return 1; /* object */
+	return 1; /* value */
 }
 
 static int luarcu_newindex(lua_State *L)
@@ -219,6 +226,13 @@ static void luarcu_release(void *private)
 		luarcu_remove(entry);
 }
 
+static inline void luarcu_inittable(luarcu_table_t *table, size_t size)
+{
+	__hash_init(table->hlist, size);
+	table->size = size;
+	table->seed = luarcu_seed();
+}
+
 static const struct luaL_Reg luarcu_lib[] = {
 	{"table", luarcu_table},
 	{NULL, NULL}
@@ -237,15 +251,23 @@ static const lunatik_class_t luarcu_class = {
 	.release = luarcu_release,
 };
 
+lunatik_object_t *luarcu_newtable(size_t size, bool sleep)
+{
+	lunatik_object_t *object;
+
+	size = roundup_pow_of_two(size);
+	if ((object = lunatik_createobject(&luarcu_class, luarcu_sizeoftable(size), sleep))!= NULL)
+		luarcu_inittable((luarcu_table_t *)object->private, size);
+	return object;
+}
+EXPORT_SYMBOL(luarcu_newtable);
+
 static int luarcu_table(lua_State *L)
 {
 	size_t size = roundup_pow_of_two(luaL_optinteger(L, 1, LUARCU_DEFAULT_SIZE));
 	lunatik_object_t *object = lunatik_newobject(L, &luarcu_class, luarcu_sizeoftable(size));
-	luarcu_table_t *table = object->private;
 
-	__hash_init(table->hlist, size);
-	table->size = size;
-	table->seed = luarcu_seed();
+	luarcu_inittable((luarcu_table_t *)object->private, size);
 	return 1; /* object */
 }
 
