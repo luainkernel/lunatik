6990af9987aead5d49f2f23dcccb259e80966979
Lourival Vieira Neto <lourival.neto@ring-0.io>
2024-05-19 21:01:22 +0000

    add packed data as argument for bpf_luaxdp_run

--- lib/luaxdp.c
+++ lib/luaxdp.c
@@ -48,29 +48,39 @@ __diag_ignore_all("-Wmissing-prototypes",
                   "Global kfuncs as their definitions will be in BTF");
 #endif
 
+static inline lunatik_object_t *luaxdp_pushdata(lua_State *L, int upvalue, void *ptr, size_t size)
+{
+	lunatik_object_t *data;
+
+	lua_pushvalue(L, lua_upvalueindex(upvalue));
+	data = (lunatik_object_t *)lunatik_toobject(L, -1);
+	luadata_reset(data, ptr, size);
+	return data;
+}
+
 static int luaxdp_callback(lua_State *L)
 {
-	lunatik_object_t *buffer;
+	lunatik_object_t *buffer, *argument;
 	struct xdp_buff *ctx = (struct xdp_buff *)lunatik_checknull(L, lua_touserdata(L, 1));
+	void *arg = lunatik_checknull(L, lua_touserdata(L, 2));
+	size_t arg__sz = (size_t)lua_tointeger(L, 3);
 
 	lua_pushvalue(L, lua_upvalueindex(1)); /* callback */
-	lua_insert(L, -3); /* stack: callback, ctx, offset */
-
-	lua_pushvalue(L, lua_upvalueindex(2)); /* buffer */
-	buffer = (lunatik_object_t *)lunatik_checknull(L, lunatik_toobject(L, -1));
-	luadata_reset(buffer, ctx->data, ctx->data_end - ctx->data);
-	lua_replace(L, 2); /* stack: callback, buffer, offset */
+	buffer = luaxdp_pushdata(L, 2, ctx->data, ctx->data_end - ctx->data);
+	argument = luaxdp_pushdata(L, 3, arg, arg__sz);
 
 	if (lua_pcall(L, 2, 1, 0) != LUA_OK) {
 		luadata_clear(buffer);
+		luadata_clear(argument);
 		return lua_error(L);
 	}
 
 	luadata_clear(buffer);
+	luadata_clear(argument);
 	return 1;
 }
 
-static int luaxdp_handler(lua_State *L, struct xdp_buff *ctx, size_t offset)
+static int luaxdp_handler(lua_State *L, struct xdp_buff *ctx, void *arg, size_t arg__sz)
 {
 	int action = -1;
 	int status;
@@ -81,8 +91,9 @@ static int luaxdp_handler(lua_State *L, struct xdp_buff *ctx, size_t offset)
 	}
 
 	lua_pushlightuserdata(L, ctx);
-	lua_pushinteger(L, (lua_Integer)offset);
-	if ((status = lua_pcall(L, 2, 1, 0)) != LUA_OK) {
+	lua_pushlightuserdata(L, arg);
+	lua_pushinteger(L, (lua_Integer)arg__sz);
+	if ((status = lua_pcall(L, 3, 1, 0)) != LUA_OK) {
 		pr_err("%s\n", lua_tostring(L, -1));
 		goto out;
 	}
@@ -92,7 +103,7 @@ out:
 	return action;
 }
 
-__bpf_kfunc int bpf_luaxdp_run(struct xdp_md *xdp_ctx, char *key, size_t key__sz, size_t offset)
+__bpf_kfunc int bpf_luaxdp_run(char *key, size_t key__sz, struct xdp_md *xdp_ctx, void *arg, size_t arg__sz)
 {
 	lunatik_object_t *runtime;
 	struct xdp_buff *ctx = (struct xdp_buff *)xdp_ctx;
@@ -105,7 +116,7 @@ __bpf_kfunc int bpf_luaxdp_run(struct xdp_md *xdp_ctx, char *key, size_t key__sz
 		goto out;
 	}
 
-	lunatik_run(runtime, luaxdp_handler, action, ctx, offset);
+	lunatik_run(runtime, luaxdp_handler, action, ctx, arg, arg__sz);
 	lunatik_putobject(runtime);
 out:
 	return action;
@@ -121,9 +132,14 @@ static const struct btf_kfunc_id_set bpf_luaxdp_kfunc_set = {
 	.owner = THIS_MODULE,
 	.set   = &bpf_luaxdp_set,
 };
-#endif
 
 #define luaxdp_setcallback(L, i)	(lunatik_setregistry((L), (i), luaxdp_callback))
+
+static inline void luaxdp_newdata(lua_State *L)
+{
+	lunatik_object_t *data = lunatik_checknull(L, luadata_new(NULL, 0, false));
+	lunatik_cloneobject(L, data);
+}
 
 static int luaxdp_detach(lua_State *L)
 {
@@ -134,21 +150,21 @@ static int luaxdp_detach(lua_State *L)
 
 static int luaxdp_attach(lua_State *L)
 {
-	lunatik_object_t *buffer;
+	luaL_checktype(L, 1, LUA_TFUNCTION); /* callback */
+	luaxdp_newdata(L); /* buffer */
+	luaxdp_newdata(L); /* argument */
 
-	luaL_checktype(L, 1, LUA_TFUNCTION);
-
-	buffer = lunatik_checknull(L, luadata_new(NULL, 0, false));
-	lunatik_cloneobject(L, buffer);
-
-	lua_pushcclosure(L, luaxdp_callback, 2);
+	lua_pushcclosure(L, luaxdp_callback, 3);
 	luaxdp_setcallback(L, -1);
 	return 0;
 }
+#endif
 
 static const luaL_Reg luaxdp_lib[] = {
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 4, 0))
 	{"attach", luaxdp_attach},
 	{"detach", luaxdp_detach},
+#endif
 	{NULL, NULL}
 };
 
