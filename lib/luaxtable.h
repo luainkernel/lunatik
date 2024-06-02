fa63a35338fdf388a444152eeddde935a5967d47
Mohammad Shehar Yaar Tausif <sheharyaar48@gmail.com>
2024-06-02 11:12:10 +0530

    add xtable rcu and lua callback execution
    Signed-off-by: Mohammad Shehar Yaar Tausif <sheharyaar48@gmail.com>

---
+++ lib/luaxtable.h
@@ -0,0 +1,16 @@
+/*
+* SPDX-FileCopyrightText: (c) 2024 Mohammad Shehar Yaar Tausif <sheharyaar48@gmail.com>
+* SPDX-License-Identifier: MIT OR GPL-2.0-only
+*/
+
+#ifndef luaxtable_h
+#define luaxtable_h
+
+struct luaxtable_s;
+
+typedef struct luaxtable_info_s {
+	struct luaxtable_s *data __attribute__((aligned(8)));
+} luaxtable_info_t;
+
+#endif
+
