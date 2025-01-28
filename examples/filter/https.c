2c88adce6b104f17cd2dfc9577b52d2025bb1370
Lourival Vieira Neto <lourival.neto@ring-0.io>
2025-01-28 15:09:47 +0000

    tmp

--- examples/filter/https.c
+++ examples/filter/https.c
@@ -7,12 +7,17 @@
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_endian.h>
 
+#define SNI_MIN	(10)
+
 extern int bpf_luaxdp_run(char *key, size_t key__sz, struct xdp_md *xdp_ctx, void *arg, size_t arg__sz) __ksym;
 
 static char runtime[] = "examples/filter/sni";
 
 struct bpf_luaxdp_arg {
 	__u16 offset;
+	__u16 dport;
+	__u32 saddr;
+	__u32 daddr;
 } __attribute__((packed));
 
 SEC("xdp")
@@ -33,14 +38,14 @@ int filter_https(struct xdp_md *ctx)
 	if (tcp + 1 > (struct tcphdr *)data_end)
 		goto pass;
 
-	if (bpf_ntohs(tcp->dest) != 443 || !tcp->psh)
-		goto pass;
-
 	void *payload = (void *)tcp + (tcp->doff * 4);
-	if (payload > data_end)
+	if (bpf_ntohs(tcp->dest) != 443 || payload > data_end - SNI_MIN)
 		goto pass;
 
 	arg.offset = bpf_htons((__u16)(payload - data));
+	arg.dport = tcp->dest;
+	arg.saddr = ip->saddr;
+	arg.daddr = ip->daddr;
 
 	int action = bpf_luaxdp_run(runtime, sizeof(runtime), ctx, &arg, sizeof(arg));
 	return action < 0 ? XDP_PASS : action;
--- examples/filter/sni.lua
+++ examples/filter/sni.lua
@@ -1,11 +1,14 @@
 --
--- SPDX-FileCopyrightText: (c) 2024 Ring Zero Desenvolvimento de Software LTDA
+-- SPDX-FileCopyrightText: (c) 2024-2025 Ring Zero Desenvolvimento de Software LTDA
 -- SPDX-License-Identifier: MIT OR GPL-2.0-only
 --
 
-local xdp  = require("xdp")
+local xdp = require("xdp")
+local linux = require("linux")
 
 local action = xdp.action
+
+local ntoh16, ntoh32 = linux.ntoh16, linux.ntoh32
 
 local function set(t)
 	local s = {}
@@ -22,57 +25,101 @@ local function log(sni, verdict)
 end
 
 local function unpacker(packet, base)
-	local byte = function (offset)
-		return packet:getbyte(base + offset)
+	base = base or 0
+
+	local u8 = function (offset)
+		return packet:getuint8(base + offset)
 	end
 
-	local short = function (offset)
-		local offset = base + offset
-		return packet:getbyte(offset) << 8 | packet:getbyte(offset + 1)
+	local be16 = function (offset)
+		return ntoh16(packet:getuint16(base + offset))
+	end
+
+	local be32 = function (offset)
+		return ntoh32(packet:getuint32(base + offset))
 	end
 
 	local str = function (offset, length)
 		return packet:getstring(base + offset, length)
 	end
 
-	return byte, short, str
+	return str, u8, be16, be32
 end
 
-local function offset(argument)
-	return select(2, unpacker(argument, 0))(0)
+local function argparser(argument)
+	local _, _, be16, be32 = unpacker(argument)
+	local offset = be16(0)
+	local conn = string.format("%X:%X:%X", be16(2), be32(4), be32(10))
+	return offset, conn
 end
 
 local client_hello = 0x01
 local handshake    = 0x16
 local server_name  = 0x00
+local host_name    = 0x00
 
 local session = 43
 local max_extensions = 17
 
-local function filter_sni(packet, argument)
-	local byte, short, str = unpacker(packet, offset(argument))
+function segment(packet, offset)
+	local str, u8, be16 = unpacker(packet, offset)
 
-	if byte(0) ~= handshake or byte(5) ~= client_hello then
+	local last = #packet - offset - 1
+	for i = 0, last do
+		local extension_type   = be16(i)
+		local server_name_type = u8(i + 6)
+
+		if extension_type == server_name and server_name_type == host_name then
+			local extension_len        = be16(i + 2)
+			local server_name_list_len = be16(i + 4)
+			local server_name_len      = be16(i + 7)
+
+			if extension_len == server_name_list_len + 2 and
+			   extension_len == server_name_len + 5 then
+				return str(i + 9, server_name_len)
+			end
+		end
+	end
+end
+
+local pending = {}
+
+local function filter_sni(packet, argument)
+	local offset, conn = argparser(argument)
+
+	if pending[conn] then
+		pending[conn] = nil
+		return segment(packet, offset)
+	end
+
+	local str, u8, be16 = unpacker(packet, offset)
+	if u8(0) ~= handshake or u8(5) ~= client_hello then
 		return action.PASS
 	end
 
-	local cipher = (session + 1) + byte(session)
-	local compression = cipher + 2 + short(cipher)
-	local extension = compression + 3 + byte(compression)
+	pending[conn] = true
+
+	local cipher = (session + 1) + u8(session)
+	local compression = cipher + 2 + be16(cipher)
+	local extension = compression + 3 + u8(compression)
 
 	for i = 1, max_extensions do
 		local data = extension + 4
-		if short(extension) == server_name then
-			local length = short(data + 3)
+		if be16(extension) == server_name then
+			pending[conn] = nil
+
+			local length = be16(data + 3)
 			local sni = str(data + 5, length)
 
 			verdict = blacklist[sni] and "DROP" or "PASS"
 			log(sni, verdict)
 			return action[verdict]
 		end
-		extension = data + short(extension + 2)
+		extension = data + be16(extension + 2)
 	end
 
+	pending[conn] = nil
+	log("unknown", "PASS")
 	return action.PASS
 end
 
