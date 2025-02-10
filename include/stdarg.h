bc57b27fc89cd59063bbcb77e527996624ddc0e4
Lourival Vieira Neto <lourival.neto@ring-0.io>
2025-02-10 12:52:14 +0000

    add support for kernel v5 on include/stdarg.h

--- include/stdarg.h
+++ include/stdarg.h
@@ -1,12 +1,18 @@
 /*
-* SPDX-FileCopyrightText: (c) 2023-2024 Ring Zero Desenvolvimento de Software LTDA
+* SPDX-FileCopyrightText: (c) 2023-2025 Ring Zero Desenvolvimento de Software LTDA
 * SPDX-License-Identifier: MIT OR GPL-2.0-only
 */
 
 #ifndef lunatik_stdarg_h
 #define lunatik_stdarg_h
 
+#include <linux/version.h>
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 15, 0)
 #include <linux/stdarg.h>
+#else
+#include <stdarg.h>
+#endif
 
 #endif
 
