031f6c5047a7b4cc431771c845787a216f9f0908
qrsikno2 <qrsikno@163.com>
2025-07-12 23:02:33 +0800

    fixup! add callback for report_descriptor

--- lib/luahid.c
+++ lib/luahid.c
@@ -13,7 +13,7 @@
 
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 #include <linux/version.h>
-#include <string.h>
+#include <linux/string.h>
 #include <linux/hid.h>
 #include <lunatik.h>
 #include <lauxlib.h>
