6bfe199524c25a2355c629a919dab529b1fc7cb0
Lourival Vieira Neto <lourival.neto@ringzero.com.br>
2025-11-25 23:59:55 +0000

    add support to connect on socket.unix

--- lib/socket/unix.lua
+++ lib/socket/unix.lua
@@ -1,6 +1,6 @@
 --
 -- SPDX-FileCopyrightText: (c) 2025 Ring Zero Desenvolvimento de Software LTDA
--- SPDX-License-Identifier: MIT OR GPL-2.0-only 
+-- SPDX-License-Identifier: MIT OR GPL-2.0-only
 --
 
 ---
@@ -17,20 +17,34 @@ local sock = socket.sock
 
 local unix = {}
 
+local function new(method)
+	unix[method] = function (path, type)
+		local type = type and string.upper(type) or "STREAM"
+		local sock = socket.new(af.UNIX, sock[type], 0)
+		sock[method](sock, path)
+		return sock
+	end
+end
+
 ---
--- Binds a new UNIX domain socket to a specific path.
+-- Creates and binds a new UNIX domain socket to a specific path.
 -- @param path (string) The UNIX domain socket path.
 -- @param type (string) [optional] "STREAM" or "DGRAM".
 -- @return A new UNIX domain socket object (stream by default) bound to path.
 -- @raise Error if socket.new() or socket.bind() fail.
 -- @see socket.new
 -- @see socket.bind
-function unix.bind(path, _type)
-	local t = _type and string.upper(_type) or "STREAM"
-	local s = socket.new(af.UNIX, sock[t], 0)
-	s:bind(path)
-	return s 
-end
+new("bind")
+
+---
+-- Creates and connects a new UNIX domain socket to a specific path.
+-- @param path (string) The UNIX domain socket path.
+-- @param type (string) [optional] "STREAM" or "DGRAM".
+-- @return A new UNIX domain socket object (stream by default) connected to path.
+-- @raise Error if socket.new() or socket.connect() fail.
+-- @see socket.new
+-- @see socket.connect
+new("connect")
 
 return unix
 
