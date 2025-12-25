73a1d0ecf9508b5021bf8ba54681f7a4a7a3b165
Lourival Vieira Neto <lourival.neto@ringzero.com.br>
2025-12-25 12:59:44 +0000

    refactor luahid

--- examples/gesture.lua
+++ examples/gesture.lua
@@ -1,67 +1,69 @@
 --
--- SPDX-FileCopyrightText: (c) 2025 Jieming Zhou <qrsikno@gmail.com>
+-- SPDX-FileCopyrightText: (c) 2025-2026 Jieming Zhou <qrsikno@gmail.com>
 -- SPDX-License-Identifier: MIT OR GPL-2.0-only
 --
 
--- Driver for Qemu's USB mouse with gesture(dragging),
--- swipeing up to lock the mouse and swipeing down to unlock it.
+-- Driver for QEMU's USB mouse with gesture (dragging),
+-- swiping right to lock the mouse and swiping left to unlock it.
 
 local hid = require("hid")
 
-local driver = {
-	name = "gesture",
-	id_table = {
-		{ vendor = 0x0627, product = 0x0001 }
-	}
-}
+local driver = {name = "gesture", id_table = {{vendor = 0x0627, product = 0x0001}}}
 
-function driver:probe(devid)
-	return { x = 0, y = 0, drag = false, lock = false }
+local threshold = 2
+
+local function debug(fmt, ...)
+	print(string.format(fmt, ...))
 end
 
-function driver:raw_event(hdev, state, report, raw_data)
-	local btn = raw_data:getbyte(0)
-	local dx_byte = raw_data:getbyte(1)
-	local dy_byte = raw_data:getbyte(2)
-	-- complement conversion
-	local dx = dx_byte >= 128 and dx_byte - 256 or dx_byte
-	local dy = dy_byte >= 128 and dy_byte - 256 or dy_byte
+function driver:probe(id)
+	self.state = {x = 0, count = 0, lock = false}
+end
 
-	local left_down = (btn & 1) == 1
-	if left_down then
-		state.x = state.x + dx
-		state.y = state.y + dy
-		state.drag = true
-	else
-		if state.drag then
-			if math.abs(state.x) > 100 or math.abs(state.y) > 100 then
-				local direction = ""
-				if math.abs(state.x) > math.abs(state.y) then
-					direction = state.x > 0 and "rightly" or "leftly"
-				else
-					direction = state.y > 0 and "downly" or "uply"
-				end
-				print(string.format("Swipe %s with x=%d, y=%d", direction, state.x, state.y))
-				if direction == "uply" then
-					state.lock = true
-					print("Locking mouse")
-				end
-				if direction == "downly" and state.lock then
-					state.lock = false
-					print("Unlocking mouse")
-				end
-			end
-			state.x = 0
-			state.y = 0
-			state.drag = false
+local function forward(x0, x1)
+	return (x0 >= 32767 and x1 <= 0) or (x1 > x0)
+end
+
+local function count(state, direction)
+	if direction ~= "neutral" then
+		if state.direction == direction then
+			state.count = state.count + 1
+		else
+			state.count = 0
+			state.direction = direction
 		end
 	end
-	if state.lock then
-		raw_data:setbyte(0, 0) -- buttons
-		raw_data:setbyte(1, 0) -- dx
-		raw_data:setbyte(2, 0) -- dy
+	return state.count
+end
+
+function driver:raw_event(hdev, report, raw)
+	local state = self.state
+	local button = raw:getbyte(0)
+
+	local left_down = (button & 1) == 1
+	if left_down then
+		local x0 = state.x
+		local x1 = raw:getint16(1)
+		local direction = x0 == x1 and "neutral" or
+			forward(x0, x1) and "right" or "left"
+
+		debug("%s\t%d %d", direction, x0, x1)
+		if count(state, direction) > threshold then
+			if direction == "right" then
+				state.lock = true
+			elseif direction == "left" then
+				state.lock = false
+			end
+		end
+		state.x = x1
 	end
-	return false
+
+	if state.lock then
+		local last = #raw - 1
+		for i = 0, last do 
+			raw:setbyte(i, 0)
+		end
+	end
 end
 
 hid.register(driver)
