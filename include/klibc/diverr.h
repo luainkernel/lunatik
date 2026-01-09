242b8b4e96ab328da4f5e18858eb733e8dca55b3
Lourival Vieira Neto <lourival.neto@ringzero.com.br>
2026-01-09 11:41:46 +0000

    add support for 64-bit integer on 32-bit platforms
    * use klibc/libgcc implementation

---
+++ include/klibc/diverr.h
@@ -0,0 +1,14 @@
+/*
+* SPDX-FileCopyrightText: (c) 2026 Ring Zero Desenvolvimento de Software LTDA
+* SPDX-License-Identifier: MIT OR GPL-2.0-only
+*/
+
+#ifndef lunatik_klibc_diverr_h
+#define lunatik_klibc_diverr_h
+
+#include <linux/types.h>
+
+static __inline__ void __divide_error(void) {}
+
+#endif
+
