553f1aa6f7dcea8ab8a47008d804bda12a4961f8
Ashwani Kumar Kamal <ashwanikamal.im421@gmail.com>
2026-05-05 22:10:50 +0530

    lunatik: introduce generic bpf execution api
    Signed-off-by: Ashwani Kumar Kamal <ashwanikamal.im421@gmail.com>

---
+++ lib/lunatik_bpf.h
@@ -0,0 +1,20 @@
+/* SPDX-FileCopyrightText: (c) 2026 Ashwani Kumar Kamal <ashwanikamal.im421@gmail.com>
+ * SPDX-License-Identifier: MIT OR GPL-2.0-only
+ */
+#ifndef LUNATIK_BPF_H
+#define LUNATIK_BPF_H
+
+#include "lunatik.h"
+
+typedef int (*lunatik_bpf_push_t)(lua_State *L, void *ctx);
+
+struct lunatik_bpf_call {
+	lua_CFunction		registry_key;
+	lunatik_bpf_push_t	push_ctx;
+	void				*ctx;
+};
+
+int lunatik_bpf_run(lunatik_object_t *runtime, lua_CFunction registry_key, lunatik_bpf_push_t push_ctx, void *ctx);
+
+#endif
+
