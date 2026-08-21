eb214e1042b1c49b3fe743b1145a7a916aa70936
Lourival Vieira Neto <lourival.neto@ringzero.com.br>
2026-08-21 12:06:54 +0000

    fixup! tests: add tc suite

--- tests/tc/attach_sleepable.lua
+++ tests/tc/attach_sleepable.lua
@@ -1,5 +1,5 @@
 --
--- SPDX-FileCopyrightText: (c) 2026 Ashwani Kumar Kamal <ashwanikamal.im421@gmail.com
+-- SPDX-FileCopyrightText: (c) 2026 Ashwani Kumar Kamal <ashwanikamal.im421@gmail.com>
 -- SPDX-License-Identifier: MIT OR GPL-2.0-only
 --
 -- Kernel-side script for the tc verdict test (see test_tc.sh).
