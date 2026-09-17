daaddc5ff985da0e97d1c293e279cb9ee1d3f77a
Lourival Vieira Neto <lourival.neto@gmail.com>
2026-09-17 08:57:15 -0300

    fixup! lunatik_conf: fence the kernel-only part of the configuration

--- lunatik_conf.h
+++ lunatik_conf.h
@@ -6,29 +6,19 @@
 #ifndef lunatik_conf_h
 #define lunatik_conf_h
 
-/* shared by the kernel and the host compiler (bin/lunatikc): everything that shapes bytecode */
+/* shared with the host compiler (bin/lunatikc): the number configuration shapes the bytecode */
 #define LUAI_UACNUMBER		LUA_INTEGER
 #define LUA_NUMBER		LUA_INTEGER
 #define LUA_NUMBER_FMT		LUA_INTEGER_FMT
 
 #define l_randomizePivot(L)	(~0)
 
-#undef lua_getlocaledecpoint
-#define lua_getlocaledecpoint()		('.')
-
-/* frame size shouldn't be larger than 1024 bytes; thus, LUAL_BUFFERSIZE
- * must be adjusted for the stack of functions that use luaL_Buffer */
-#undef LUAL_BUFFERSIZE
-#define LUAL_BUFFERSIZE		(256) /* {laux,load,lstr,ltab,lutf8}lib.c */
-
-#undef LUAI_MAXSTACK
-#define LUAI_MAXSTACK  200
-
-#define LUNATIK_GCCOUNT	/* LUA_GCCOUNT in bytes, instead of Kbytes */
-
-#ifdef __KERNEL__
+#ifdef __KERNEL__ /* _KERNEL, the fork's dialect switch, is set on the host build too */
 #include <linux/random.h>
 #define luai_makeseed(L)		get_random_u32()
+
+#undef lua_getlocaledecpoint
+#define lua_getlocaledecpoint()		('.')
 
 #define lua_writestring(s,l)		printk("%s",(s))
 #define lua_writeline()			pr_cont("\n")
@@ -36,6 +26,11 @@
 
 /* see https://www.gnu.org/software/libc/manual/html_node/Atomic-Types.html */
 #define l_signalT	int
+
+/* frame size shouldn't be larger than 1024 bytes; thus, LUAL_BUFFERSIZE
+ * must be adjusted for the stack of functions that use luaL_Buffer */
+#undef LUAL_BUFFERSIZE
+#define LUAL_BUFFERSIZE		(256) /* {laux,load,lstr,ltab,lutf8}lib.c */
 
 #ifdef lauxlib_c
 #define panic	lua_panic
@@ -77,6 +72,9 @@ int lunatik_loadfile(lua_State *L, const char *filename, const char *mode);
 #undef LUA_PATH_DEFAULT
 #define LUA_PATH_DEFAULT  LUA_ROOT"?.lua;" LUA_ROOT"?/init.lua"
 
+#undef LUAI_MAXSTACK
+#define LUAI_MAXSTACK  200
+
 /* stored in L's extraspace; gates lunatik_run */
 struct lunatik_object_s;
 typedef struct lunatik_runtime_s {
@@ -93,11 +91,14 @@ unsigned int luaS_hash(const char *str, size_t l, unsigned int seed); /* require
 #define	lunatik_hash(str, l, seed)	luaS_hash((str), (l), (seed))
 #endif /* LUNATIK_RUNTIME */
 
+#define LUNATIK_GCCOUNT	/* LUA_GCCOUNT in bytes, instead of Kbytes */
+
 #if defined(lcode_c) || defined(ldebug_c) || defined(llex_c) || defined(lparser_c) || defined(lstate_c)
 #ifdef current /* defined by asm/current.h */
 #undef current /* conflicts with Lua namespace */
 #endif
 #endif
+
 #endif /* __KERNEL__ */
 
 #endif
