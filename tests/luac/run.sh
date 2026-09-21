677d0309fad6aac653ec06953c14e56703954e91
Lourival Vieira Neto <lourival.neto@ringzero.com.br>
2026-09-21 10:11:44 -0300

    fixup! tests: add the luac suite

--- tests/luac/run.sh
+++ tests/luac/run.sh
@@ -21,6 +21,7 @@ SRC="$SCRIPTS_PATH/tests/luac"
 source "$(dirname "$(readlink -f "$0")")/../lib.sh"
 
 cleanup() {
+	for s in hello_bc hello_s user textmode; do lunatik stop "tests/luac/$s" 2>/dev/null; done
 	rm -f "$SRC"/*_bc.lua "$SRC"/*_s.lua "$SRC"/stock.lua
 }
 trap cleanup EXIT
